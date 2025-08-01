4c84f81f27ff4eefd9455b9242d349a29de1188a
Aaditya Gupta <168216358+deltay-deltax@users.noreply.github.com>
2025-08-01 21:25:53 +0530

    Update aqidata.dart

--- lib/AQI_Service/aqidata.dart
+++ lib/AQI_Service/aqidata.dart
@@ -6,7 +6,7 @@ class AQIService {
     required double longitude,
   }) async {
     try {
-      final String apikey = '4c0da3560ead6dfd6901ff0b5b7116f8';
+      final String apikey = 'your api key';
       var response = await Dio().get('your api key ');
 
       final data = response.data['list'][0];
